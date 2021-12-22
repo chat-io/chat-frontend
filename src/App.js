1d6fb460cfe2dbd7cd1deb651f19d1c6f7644a5f
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-23 01:04:33 +0900

    feat: add react-rotuer
    - react router 추가

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import Chat from "./components/Chat/Chat";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Signup />
-      <Chat />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route render={() => <h1>404 page not found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
