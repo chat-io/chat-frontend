e7475e06491b25bf2ac671920cbcba088572df48
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-23 14:09:04 +0900

    feat: Login component 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import Chat from "./components/Chat/Chat";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className={styles.App}>
         <Routes>
           <Route exact path="/" element={<Chat />} />
           <Route path="/login" element={<Login />} />
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,36 @@
 import React from "react";
 
+import styles from "./Auth.module.css";
+
+import loginImage from "../../assets/images/login.svg";
+
 const Login = () => {
-  return <h1>Login screen</h1>;
+  return (
+    <div className={styles["auth-container"]}>
+      <div className={styles["auth-card"]}>
+        <div>
+          <div className={styles["image-section"]}>
+            <img src={loginImage} alt="Login" />
+          </div>
+
+          <div className={styles["form-section"]}>
+            <h2>Welcome back</h2>
+            <form>
+              <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
+                <input placeholder="Email" />
+              </div>
+              <div className={`${styles["input-field"]} ${styles["mb-2"]}`}>
+                <input placeholder="Email" />
+              </div>
+              <button>LOGIN</button>
+            </form>
+
+            <p>Don't have an account? Register</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
