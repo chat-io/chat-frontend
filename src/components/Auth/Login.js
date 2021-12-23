39b7f26dc189ae7c2b1b0c0c281ba6650ca315a8
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-23 23:24:34 +0900

    feat: Login Component states 추가
    - email
    - password

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Auth.module.css";
 
 import loginImage from "../../assets/images/login.svg";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  // const loginHandler = () => {};
+
   return (
     <div className={styles["auth-container"]}>
       <div className={styles["auth-card"]}>
@@ -15,12 +33,24 @@ const Login = () => {
 
           <div className={styles["form-section"]}>
             <h2>Welcome back</h2>
-            <form>
+            <form onSubmit={submitHandler}>
               <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
-                <input placeholder="Email" />
+                <input
+                  onChange={emailChangeHandler}
+                  value={email}
+                  required={true}
+                  type="text"
+                  placeholder="Email"
+                />
               </div>
               <div className={`${styles["input-field"]} ${styles["mb-2"]}`}>
-                <input placeholder="Password" />
+                <input
+                  onChange={passwordChangeHandler}
+                  value={password}
+                  required={true}
+                  type="password"
+                  placeholder="Password"
+                />
               </div>
               <button>LOGIN</button>
             </form>
