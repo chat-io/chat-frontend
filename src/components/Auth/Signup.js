ef2cb60f308e79cce2aac006b4f74d35ba57dbe3
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-23 14:38:48 +0900

    feat: Signup component 구현

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,7 +1,52 @@
 import React from "react";
 
+import styles from "./Auth.module.css";
+
+import signupImage from "../../assets/images/signup.svg";
+
 const Signup = () => {
-  return <h1>Signup screen</h1>;
+  return (
+    <div className={styles["auth-container"]}>
+      <div className={styles["auth-card"]}>
+        <div>
+          <div className={styles["image-section"]}>
+            <img src={signupImage} alt="Signup" />
+          </div>
+
+          <div className={styles["form-section"]}>
+            <h2>Create an account</h2>
+            <form>
+              <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
+                <input placeholder="First name" />
+              </div>
+              <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
+                <input placeholder="Last name" />
+              </div>
+
+              <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
+                <input placeholder="Email" />
+              </div>
+
+              <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
+                <select>
+                  <option value="male">Male</option>
+                  <option value="femail">Female</option>
+                </select>
+              </div>
+
+              <div className={`${styles["input-field"]} ${styles["mb-2"]}`}>
+                <input placeholder="Password" />
+              </div>
+
+              <button>REGISTER</button>
+            </form>
+
+            <p>Already have an account? Login.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
