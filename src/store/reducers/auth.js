fb61e04351da6b840e084dc9ec4df3e87511764d
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-25 02:44:38 +0900

    feat: login 상태 유지 기능 구현
    - isLoggedIn 설정 전 검증과정 추가 필요(?)

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,28 +1,28 @@
 import { LOGIN, SIGN_UP, LOGOUT } from "../actions/auth";
 
 const initialState = {
-  user: {},
-  token: "",
-  isLoggedIn: false,
+  user: JSON.parse(localStorage.getItem("user")) || {},
+  token: localStorage.getItem("token") || "",
+  isLoggedIn: !!localStorage.getItem("user"),
+  //isLoggedIn 설정하기 위해 token 검증과정이 필요하지 않은가?
 };
 
 const authReducer = (state = initialState, action) => {
   const { type, payload } = action;
-
   switch (type) {
     case LOGIN:
       return {
         ...state,
-        user: payload.data.login.user,
-        token: payload.data.login.token,
+        user: payload.user,
+        token: payload.token,
         isLoggedIn: true,
       };
 
     case SIGN_UP:
       return {
         ...state,
-        user: payload.data.signup.user,
-        token: payload.data.signup.token,
+        user: payload.user,
+        token: payload.token,
         isLoggedIn: true,
       };
 
