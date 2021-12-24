4c04c74843d9d3c1a084c7676d189fb58aa71b52
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-24 17:23:56 +0900

    feat: input change handler 구현

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./Auth.module.css";
+import { useDispatch } from "react-redux";
 
+import styles from "./Auth.module.css";
 import signupImage from "../../assets/images/signup.svg";
 
 const Signup = () => {
+  const dispatch = useDispatch();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("male");
+  const [password, setPassword] = useState("");
+
+  dispatch();
+
+  const firstNameChangeHandler = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const genderChangeHandler = (event) => {
+    setGender(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className={styles["auth-container"]}>
       <div className={styles["auth-card"]}>
@@ -14,33 +45,60 @@ const Signup = () => {
           </div>
 
           <div className={styles["form-section"]}>
-            <h2>Create an account</h2>
+            <h2>Create an account</h2> 
             <form>
               <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
-                <input placeholder="First name" />
+                <input
+                  onChange={firstNameChangeHandler}
+                  value={firstName}
+                  required={true}
+                  type="text"
+                  placeholder="First name"
+                />
               </div>
               <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
-                <input placeholder="Last name" />
+                <input
+                  onChange={lastNameChangeHandler}
+                  value={lastName}
+                  required={true}
+                  type="text"
+                  placeholder="Last name"
+                />
               </div>
 
               <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
-                <input placeholder="Email" />
+                <input
+                  onChange={emailChangeHandler}
+                  value={email}
+                  required={true}
+                  type="email"
+                  placeholder="Email"
+                />
               </div>
 
               <div className={`${styles["input-field"]} ${styles["mb-1"]}`}>
-                <select>
+                <select
+                  onChange={genderChangeHandler}
+                  value={gender}
+                  required={true}
+                >
                   <option value="male">Male</option>
                   <option value="femail">Female</option>
                 </select>
               </div>
 
               <div className={`${styles["input-field"]} ${styles["mb-2"]}`}>
-                <input placeholder="Password" />
+                <input
+                  onChange={passwordChangeHandler}
+                  value={password}
+                  required={true}
+                  type="password"
+                  placeholder="Password"
+                />
               </div>
 
               <button>REGISTER</button>
             </form>
-
             <p>
               Already have an account? <Link to="/login">Login.</Link>
             </p>
